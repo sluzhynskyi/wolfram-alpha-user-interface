e92e899db6eec28b52c96c5788c5be5529d08866
Danylo Sluzhynskyi <44615981+wat4era@users.noreply.github.com>
2019-04-20 17:46:52 +0300

    Delete test1.py

--- examples/api_tests/test1.py
+++
@@ -1,8 +0,0 @@
-import wolframalpha  as wf
-
-client = wf.Client("JYETQ2-WP5J5GX3ER")  # app_ID is the app id
-
-q = "Solve 274770*x**1.2 + 8271.8*x**2.2 + 79.555*x**3.2 + 0.22184*x**4.2 - 35435557377 = 0"
-res = client.query(q)
-answer = next(res.results).text
-print(answer)
